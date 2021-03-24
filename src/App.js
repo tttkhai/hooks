a07ba1455ed0d22d844b22a3a2c29c424a2b2b14
Khai Tran <khaitran@Khais-MacPro.local>
2021-03-24 14:17:13 -0400

    use useContext

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
-import MainApp from './components/MainApp'
+import "./App.css";
+import MainApp from "./components/MainApp";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <MainApp />
+      <Router>
+        <nav>
+          {/* <ul> */}
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/app">Main App</Link>
+            </li>
+          {/* </ul> */}
+        </nav>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/app" component={MainApp} />
+        {/* <MainApp /> */}
+      </Router>
     </div>
   );
 }
