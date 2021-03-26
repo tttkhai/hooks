4f290749db44882ca7aa35de644537188bfcfe38
Khai <khai_tran2@apple.com>
2021-03-26 16:24:37 -0400

    used useContext

--- src/App.js
+++ src/App.js
@@ -3,28 +3,39 @@ import MainApp from "./components/MainApp";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
+import { UseContext } from "./components/UseContext";
+import React, { useEffect, useState } from "react";
+import { login } from "./services/login";
+import { getFullName } from "./components/useFetch";
 
 function App() {
+  const [user, setUser] = useState({ id: null, first: null, last: null });
+  // useEffect(() => {
+  //   var nameObject = login();
+  //   setName(getFullName(nameObject));
+  //   console.log("check" + JSON.stringify(name));
+  // }, []);
   return (
     <div className="App">
       <Router>
         <nav>
           {/* <ul> */}
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/app">Main App</Link>
-            </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/app">Main App</Link>
+          </li>
           {/* </ul> */}
         </nav>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/app" component={MainApp} />
-        {/* <MainApp /> */}
+        <UseContext.Provider value={{ user, setUser }}>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/app" component={MainApp} />
+        </UseContext.Provider>
       </Router>
     </div>
   );
---
+++ src/services/login.js
@@ -0,0 +1,15 @@
+export const login=()=>{
+    return {
+        id: 1,
+        first: "Khai",
+        last: "Tran",
+    };
+}
+
+export const logout=()=>{
+    return {
+      id: null,
+      first: null,
+      last: null,
+    };
+}
